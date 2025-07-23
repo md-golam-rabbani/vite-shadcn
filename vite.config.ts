e7c2835ce0eb0120139a40fc6d5e477acf6c4806
md-golam-rabbani <rabbani.cse.69@gmail.com>
2025-07-23 23:39:43 +0600

    chore: Add sidebar component

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,21 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    checker({
+      typescript: true,
+      // TODO: Will fix this later
+      // eslint: {
+      //   lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      // },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
